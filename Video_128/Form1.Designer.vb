edc45914d21b87a7991b2776b229735a9f46a03e
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-04 21:23:14 -0500

    Finished

--- Video_128/Form1.Designer.vb
+++ Video_128/Form1.Designer.vb
@@ -22,17 +22,41 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(185, 259)
+        Me.ListBox1.TabIndex = 0
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(203, 117)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 23)
+        Me.NumericUpDown1.TabIndex = 1
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(411, 290)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.ListBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
 End Class
--- Video_128/Form1.vb
+++ Video_128/Form1.vb
@@ -1,3 +1,33 @@
 ﻿Public Class Form1
 
+    Private persArray As New ArrayList
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For intIndex As Integer = 1 To 50
+            Dim newPers As New Person
+            NumericUpDown1.Value = Person.minAge
+            newPers.pName = "Sam " & intIndex
+            persArray.Add(newPers)
+        Next
+        UpdateList()
+    End Sub
+
+    Private Sub UpdateList()
+        ListBox1.Items.Clear()
+        For Each thePerson As Person In persArray
+            ListBox1.Items.Add(thePerson.pName & " minimum age " & Person.minAge)
+        Next
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        Person.minAge = NumericUpDown1.Value
+        UpdateList()
+    End Sub
+End Class
+
+
+Public Class Person
+
+    Public pName As String
+    Public Shared minAge As Integer = 16
+
 End Class
